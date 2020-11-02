8daa5e3a6bd441739da04c0ab61896d7d114839f
tripolan94 <qkrwlsrb35@naver.com>
2020-11-02 12:04:17 +0900

    규1102

---
+++ animalMate/src/main/java/co/animalMate/board/vo/PetVO.java
@@ -0,0 +1,79 @@
+package co.animalMate.board.vo;
+
+public class PetVO {
+	private int code;
+	private String name;
+	private int age;
+	private String gender;
+	private String type;
+	private String detailType;
+	private String cut;
+	private String comm;
+	private String id;
+	private String pic;
+	
+	public PetVO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDetailType() {
+		return detailType;
+	}
+	public void setDetailType(String detailType) {
+		this.detailType = detailType;
+	}
+	public String getCut() {
+		return cut;
+	}
+	public void setCut(String cut) {
+		this.cut = cut;
+	}
+	public String getComm() {
+		return comm;
+	}
+	public void setComm(String comm) {
+		this.comm = comm;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+}
