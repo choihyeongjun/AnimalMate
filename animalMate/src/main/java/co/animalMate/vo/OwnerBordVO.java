9ba94c9130ea7ba74215100df5993a34d3c45116
seonae <seonae@kakao.com>
2020-11-04 17:48:15 +0900

    .

---
+++ animalMate/src/main/java/co/animalMate/vo/OwnerBordVO.java
@@ -0,0 +1,50 @@
+package co.animalMate.vo;
+
+public class OwnerBordVO {
+	private String pPic;
+	private String pType;
+	private String tLocation2;
+	private int tWtime;
+	private int tPrice;
+	private int tStatus;
+	
+	public String getpPic() {
+		return pPic;
+	}
+	public void setpPic(String pPic) {
+		this.pPic = pPic;
+	}
+	public String getpType() {
+		return pType;
+	}
+	public void setpType(String pType) {
+		this.pType = pType;
+	}
+	public String gettLocation2() {
+		return tLocation2;
+	}
+	public void settLocation2(String tLocation2) {
+		this.tLocation2 = tLocation2;
+	}
+	public int gettWtime() {
+		return tWtime;
+	}
+	public void settWtime(int tWtime) {
+		this.tWtime = tWtime;
+	}
+	public int gettPrice() {
+		return tPrice;
+	}
+	public void settPrice(int tPrice) {
+		this.tPrice = tPrice;
+	}
+	public int gettStatus() {
+		return tStatus;
+	}
+	public void settStatus(int tStatus) {
+		this.tStatus = tStatus;
+	}
+	
+	
+	
+}
