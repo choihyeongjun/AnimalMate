a533acd213dc8706777cd0ca6391a65f8ae9d5b7
Kim Sang Min <yhoo112@naver.com>
2020-11-09 09:39:59 +0900

    공지사항 조회수, DB수정

--- animalMate/src/main/java/co/animalMate/common/DAO.java
+++ animalMate/src/main/java/co/animalMate/common/DAO.java
@@ -8,9 +8,9 @@ public class DAO {
 
 	private String driver = "oracle.jdbc.driver.OracleDriver";
 	//학원
-		//private String url = "jdbc:oracle:thin:@192.168.0.92:1521:xe";
+		private String url = "jdbc:oracle:thin:@192.168.0.92:1521:xe";
 	//집
-		private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		//private String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		
 	private String user = "mongde";
 	private String password = "1234";
