ded255ce1291fd38fbec91e784bfc4d764f89324
tripolan94 <qkrwlsrb35@naver.com>
2020-11-12 17:57:54 +0900

    규1112 마지막

--- animalMate/src/main/java/co/animalMate/mypage/command/JoblistUpdateAction.java
+++ animalMate/src/main/java/co/animalMate/mypage/command/JoblistUpdateAction.java
@@ -2,8 +2,7 @@ package co.animalMate.mypage.command;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -14,35 +13,31 @@ import co.animalMate.board.dao.MypageDao;
 import co.animalMate.common.Action;
 import co.animalMate.common.FileRenamePolicy;
 import co.animalMate.common.FileUtil;
-import co.animalMate.login.dao.MemberDao;
 import co.animalMate.vo.JoblistVO;
-import co.animalMate.vo.MemberVO;
-import sun.security.util.Length;
 
 public class JoblistUpdateAction implements Action {
 
 	@Override
 	public String exec(HttpServletRequest request, HttpServletResponse response) {
-		// 시터가 돌봐주세요 게시판에서 체크리스트에서 업무사진을 올리고 저장하는 처리
-		MypageDao myDao = new MypageDao();
+		// 시터가 돌봐주세요 게시판에서 체크리스트에서 업무사진을 올리고 저장하는 처리		
 		JoblistVO jobVo = new JoblistVO();
-		//List<JoblistVO> joblist = new ArrayList<JoblistVO>();
-		
-		
+
 		String[] comm = request.getParameterValues("comm");
 		String[] code = request.getParameterValues("code");
-		/*
-		 * for (int i = 0; i < comm.length; i++) {
-		 * jobVo.setCode(Integer.parseInt(code[i])); jobVo.setComm(comm[i]); }
-		 */
 		
 		// 현재 컨텍스트에 사진업로드하기
 		String appPath = request.getServletContext().getRealPath("/images/joblist_pic");
 		try {
 			for (Part part : request.getParts()) { // 첨부파일을 읽어온다
-				part.getName();
 				String fileName = FileUtil.extractFileName(part); // 파일이 존재하면
 				if (!fileName.equals("")) {
+					
+					System.out.println(part.getName());
+					
+					String pici = part.getName().substring(3);
+					System.out.println(pici);
+					int i = Integer.parseInt(pici);
+					
 					String uploadFile = appPath + File.separator + fileName;
 					// 파일명 중복체크
 					File renameFile = FileRenamePolicy.rename(new File(uploadFile));
@@ -50,6 +45,10 @@ public class JoblistUpdateAction implements Action {
 					request.setAttribute("pic", renameFile.getName());
 					jobVo.setPic(renameFile.getName());
 					
+					jobVo.setCode(Integer.parseInt(code[i]));
+					jobVo.setComm(comm[i]);
+					MypageDao myDao = new MypageDao();
+					myDao.updateJoblist(jobVo);
 				}
 			}
 		} catch (IOException e) {
@@ -57,11 +56,7 @@ public class JoblistUpdateAction implements Action {
 		} catch (ServletException e) {
 			e.printStackTrace();
 		}
-		
-		myDao.updateJoblist(jobVo);
-		
-		
-		
+
 		return "mytradeList.do";
 	}
 
